45aa36bc7f6c11597cdd72ebad8fe2bea31817bb
waselrosa <rosa@waselrosa.com>
2014-12-08 11:11:27 -0800

    Users can create a product, which has a name (string), description (text / text_area) and price (integer)

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,28 @@
+class ProductsController < ApplicationController
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @products = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    @product.save
+
+    redirect_to @product, notice: "Product created successfully"
+  end
+
+  private
+  def set_product
+    @products = Product.find(params[:id])
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :description, :price)
+  end
+
+end
